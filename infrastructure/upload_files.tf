530276e7629f828ea5fdeaa8eda8c14f41d1ad1a
hiagors <hiago_reis@hotmail.com>
2022-05-12 21:43:31 -0300

    fix upload_files.tf

--- infrastructure/upload_files.tf
+++ infrastructure/upload_files.tf
@@ -2,7 +2,6 @@ resource "aws_s3_bucket_object" "bucket" {
     for_each = fileset("data/", "*")
     bucket = aws_s3_bucket.bucket.id
     key = each.value
-    sourse = "data/${each.value}"
+    source = "data/${each.value}"
     etag = filemd5("data/${each.value}")
-
 }
